1b3d15d6dde837f0519acbf0ceb8553749978809
vladimir <vladimir.drozdik@gmail.com>
2023-01-04 23:28:25 +0200

    CRUD spring boot application using Thymeleaf template engine.

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user_details")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    // standard constructors / setters / getters / toString
+}
