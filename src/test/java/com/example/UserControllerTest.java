6ba49ff98ad064b0e3ac2c972fce749af8b24a9b
Vladimir Drozdik <vladimir.drozdik@gmail.com>
2023-02-10 09:13:10 +0200

    Use 'test' profile with connection to separate database for integration tests

--- src/test/java/com/example/UserControllerTest.java
+++ src/test/java/com/example/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -16,18 +19,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Tag("integration-test")
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 @WithMockUser(username = "user")
 class UserControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Test
     void returnsExistingUserInAModel() throws Exception {
+        User user = userRepository.save(user());
+
         this.mockMvc
                 .perform(get("/index"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("users", Matchers.is(List.of(user))));
+    }
+
+    private User user() {
+        User user = new User();
+        user.setEmail(email());
+        user.setName(name());
+        return user;
+    }
+
+    private String name() {
+        return "John Snow";
+    }
+
+    private String email() {
+        return "user@email.com";
     }
 }
